8ffb5648db72264e395f8dd189498b4d927c540e
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-11-21 15:45:00 +0300

    axios added as a package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Blogs from './pages/Blogs';
 
 function App() {
   return (
     <div className="App">
       <h2>Blog app</h2>
+      <Blogs />
     </div>
   );
 }
---
+++ src/pages/Blogs.js
@@ -0,0 +1,9 @@
+
+
+export default function Blogs() {
+    return (
+        <div>
+            <h2>This is blogs page</h2>
+        </div>
+    )
+}
