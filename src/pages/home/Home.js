e95341f27555ed674458cb0f2534ed71671c60d8
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-11-22 11:55:52 +0300

    page title added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import Topbar from '../../components/topbar/Topbar';
+import './home.css';
 
 export default function Home() {
 
     return (
         <div>
             <Topbar />
+            <div className="pageTitle">
+                <span>Latest Posts</span>
+            </div>
         </div>
     )
 }
