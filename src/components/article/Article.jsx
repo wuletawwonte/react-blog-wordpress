f64bfed39a54dd727393fd600c52f3546695d224
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-11-26 14:52:22 +0300

    author name displayed per blog post

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,13 +1,47 @@
-import './article.css';
-import Editor from '../../components/editor/Editor';
+import "./article.css";
+// import Editor from "../../components/editor/Editor";
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 export default function Article(props) {
-    const singleArticle = props.blog;
-    return (<>
-            {singleArticle.featured_image_url ? <img className="articleImage" src={singleArticle.featured_image_url} alt="singleArticle.title.rendered" /> : ''}
-            <h2 dangerouslySetInnerHTML={{__html: singleArticle.title.rendered}}></h2>
-            <p dangerouslySetInnerHTML={{__html: singleArticle.excerpt.rendered.substring(0, 150)}}></p>
-            <Editor editorId={singleArticle.author} />
-            </>
-    )
+  const {title, featured_image_url, excerpt, author} = props.blog;
+  const [authorData, setAuthorData] = useState({});
+
+  useEffect(() => {
+    axios.get("/wp-json/wp/v2/users/" + author)
+      .then((res) => {
+        console.log(res.data);
+        setAuthorData({
+          isLoading: true,
+          data: res.data
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [author]);
+
+  return (
+    <>
+      {featured_image_url ? (
+        <img
+          className="articleImage"
+          src={featured_image_url}
+          alt="title.rendered"
+        />
+      ) : (
+        ""
+      )}
+      <h2
+        dangerouslySetInnerHTML={{ __html: title.rendered }}
+      ></h2>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: excerpt.rendered.substring(0, 150),
+        }}
+      ></p>
+      {authorData.isLoading ? authorData.data.name : 'Loading' } 
+      {/* <Editor editorId={author} /> */}
+    </>
+  );
 }
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,29 +1,38 @@
-import axios from 'axios';
-import {useState, useEffect} from 'react';
-import Article from '../article/Article';
-import './blogs.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Article from "../article/Article";
+import "./blogs.css";
 
-export default function Blogs() {
-    const [blogs, setBlogs] = useState([]);
+export default function Blogs() {    
+  const [blogs, setBlogs] = useState({});
 
-    useEffect(() => {
-        axios.get('/wp-json/wp/v2/posts?per_page=8')
-            .then((res) => {
-                setBlogs(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, [])
+  useEffect(() => {
+    axios
+      .get("/wp-json/wp/v2/posts?per_page=8")
+      .then((res) => {
+        setBlogs({ 
+            data: res.data, 
+            isLoaded: true 
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-        <div className="blogsContainer">
-            {blogs.length !== 0 ? blogs.map((blog) => {
-                return <div className="article" key={blog.id}>
-                    <Article blog={blog}/>
-                </div>;
-            }) : <h2>Loading...</h2>}
-            
-        </div>
-    )
+  return (
+    <div className="blogsContainer">
+      {blogs.isLoaded ? (
+        blogs.data.map((blog) => {            
+          return (
+            <div className="article" key={blog.id}>
+              <Article blog={blog} />
+            </div>
+          );
+        })
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
