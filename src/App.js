1855cffdf4472e3ec13760b1232d63b15e196b45
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-11-27 21:55:31 +0300

    login page left side layout and styling done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom";
+import "./login.css";
 
 export default function Login() {
-    return (
-        <div>
-            <h2>This is the Login Page</h2>            
+  return (
+    <div className="mainContainer">
+      <div className="leftSide">
+        <div className="leftInside">
+          <h1>Welcome to the User Login Area</h1>
+          <p>To request website Design just call us using these phone numbers</p>
+          <div className="phoneNumbers">
+            <h2>+251 926 350413</h2>
+            <h2>+251 926 350413</h2>
+          </div>
+          <Link to="/" className="linkToHome">Back to Home</Link>
         </div>
-    )
+      </div>
+      <div className="rightSide">
+        <h2>Signin here</h2>
+      </div>
+    </div>
+  );
 }
