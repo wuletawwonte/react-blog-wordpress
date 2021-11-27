70b4ca18936d6cf64061cfc355c263ba9ee4f4ac
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-11-27 23:05:38 +0300

    Login page styling done

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -7,16 +7,32 @@ export default function Login() {
       <div className="leftSide">
         <div className="leftInside">
           <h1>Welcome to the User Login Area</h1>
-          <p>To request website Design just call us using these phone numbers</p>
+          <p>
+            To request website Design just call us using these phone numbers
+          </p>
           <div className="phoneNumbers">
             <h2>+251 926 350413</h2>
             <h2>+251 926 350413</h2>
           </div>
-          <Link to="/" className="linkToHome">Back to Home</Link>
+          <Link to="/" className="linkToHome">
+            Back to Home
+          </Link>
         </div>
       </div>
       <div className="rightSide">
-        <h2>Signin here</h2>
+        <div className="rightInside">
+          <h2>Signin here</h2>
+          <p>Please enter your username and password</p>
+          <form>
+            <div className="inputContainer">
+              <input className="username" type="text" placeholder="User name" />
+            </div>
+            <div className="inputContainer">
+              <input className="password" type="password" placeholder="Password"/>
+            </div>
+            <input className="loginButton" type="submit" value="Login" />
+          </form>
+        </div>
       </div>
     </div>
   );
