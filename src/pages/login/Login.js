ce3312a0d54f559a0d788621088b26d5ae1d7e8f
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-11-28 15:34:03 +0300

    formik form validation applied in the login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
 import "./login.css";
+import { useFormik } from "formik";
 
 export default function Login() {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <div className="mainContainer">
       <div className="leftSide">
@@ -23,12 +34,34 @@ export default function Login() {
         <div className="rightInside">
           <h2>Signin here</h2>
           <p>Please enter your username and password</p>
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <div className="inputContainer">
-              <input className="username" type="text" placeholder="User name" />
+              <input
+                id="username"
+                name="username"
+                className="username"
+                type="text"
+                placeholder="User name"
+                onChange={formik.handleChange}
+                value={formik.values.username}
+              />
+              {formik.errors.username ? (
+                <div>{formik.errors.username}</div>
+              ) : null}
             </div>
             <div className="inputContainer">
-              <input className="password" type="password" placeholder="Password"/>
+              <input
+                id="password"
+                name="password"
+                className="password"
+                type="password"
+                placeholder="Password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+              />
+              {formik.errors.password ? (
+                <div>{formik.errors.password}</div>
+              ) : null}
             </div>
             <input className="loginButton" type="submit" value="Login" />
           </form>
@@ -38,3 +71,16 @@ export default function Login() {
     </div>
   );
 }
+
+const validate = (values) => {
+  const errors = {};
+  if (!values.username) {
+    errors.username = "Required";
+  }
+
+  if (!values.password) {
+    errors.password = "Required";
+  }
+
+  return errors;
+};
