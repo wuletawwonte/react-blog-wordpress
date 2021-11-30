30b154ab2b033e0c49c9771fee77460a3a31b845
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-11-30 23:03:11 +0300

    useContext and createContext api properly work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import routes from "./config/routes";
+import { UserProvider } from "./context/UserProvider";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {routes.map((route) => {
-          return <Route key={route.path} path={route.path} element={route.component} />;
-        })}
-      </Routes>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.component}
+              />
+            );
+          })}
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 }
 
---
+++ src/context/UserProvider.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import UserContext from "./userContext";
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
