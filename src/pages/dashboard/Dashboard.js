3e9426c2efc1fa82061d37ad021785d930a0f78f
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-01 08:37:08 +0300

    dashboard page displays data from context

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,16 @@
 import './dashboard.css';
+import {useContext} from 'react';
+import UserContext from '../../context/userContext';
 
 export default function Dashboard() {
+    const {user, setUser} = useContext(UserContext);
+
     return (
         <div>
-            <h2>Dashboard Page</h2>            
+            <h2>Dashboard Page</h2>
+            <pre>{user ? user.name: <button onClick={() => {
+                setUser({name: "Wuletaw Wonte"});
+            }}>Add User</button>}</pre>            
         </div>
     )
 }
