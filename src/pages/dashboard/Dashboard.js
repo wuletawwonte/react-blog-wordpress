a720588a67644ab3ce32e154ad304da8913c70db
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-02 22:19:20 +0300

    dashboard page header styling done

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,20 +1,37 @@
-import './dashboard.css';
-import {useContext} from 'react';
-import UserContext from '../../context/UserContext';
-import { useNavigate } from 'react-router';
+import "./dashboard.css";
+import { useContext } from "react";
+import UserContext from "../../context/UserContext";
+import { useNavigate } from "react-router";
 
 export default function Dashboard() {
-    const {user, setUser} = useContext(UserContext);
-    const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
-    return (
-        <div>
-            <h2>Dashboard Page</h2>
-            <pre>Welcome {user.name}!</pre>
-            <button onClick={() => {
-                setUser({isLoggedIn: false, name: null});
-                navigate("/login");
-            }}>Logout</button>            
+  return (
+    <>
+      <div className="header">
+        <h2>Gracesoft</h2>
+        <div className="search">
+          <input type="text" placeholder="search..." />
         </div>
-    )
+        <div className="rightHeader">
+          <span>
+            Welcome <b>{user.name}</b>
+          </span>
+
+          <button
+            onClick={() => {
+              setUser({ isLoggedIn: false, name: null });
+              navigate("/login");
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+      <div className="dashboardContent">
+        <h2>Dashboard Page</h2>
+      </div>
+    </>
+  );
 }
