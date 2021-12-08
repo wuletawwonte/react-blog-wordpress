c265721c8a9823ca07cb2242304802c2f76ef508
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-09 00:02:12 +0300

    :children_crossing: side menu added and styled

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Post from "../../components/post/Post";
 import AdminNavbar from "../../components/adminNavbar/AdminNavbar";
 import "./dashboard.css";
-
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
   const [posts, setPosts] = useState({});
@@ -23,15 +23,32 @@ export default function Dashboard() {
     <>
       <AdminNavbar />
       <div className="container">
-        <h2>Dashboard Page</h2>
-        <div className="row container">
-          <div className="col-3">column one</div>
-          <div className="col-9">
+        <div className="dashboard-container">
+          <div className="item side-menu">
+            <ul className="menu">
+              <li className="menu-item">
+                <Link to="/" className="menu-item-link">Dashboard</Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/" className="menu-item-link">Add Post</Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/" className="menu-item-link">Pages</Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/" className="menu-item-link">Settings</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="item main-content">
             {posts.isLoaded
               ? posts.data.map((post) => {
                   return (
                     <div key={post.id}>
-                      <Post title={post.title.rendered} excerpt={post.excerpt.rendered} />
+                      <Post
+                        title={post.title.rendered}
+                        excerpt={post.excerpt.rendered}
+                      />
                     </div>
                   );
                 })
