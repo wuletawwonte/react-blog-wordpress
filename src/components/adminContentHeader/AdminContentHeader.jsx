d64f601c49ddeae9d88fc25b096177c20c2b1d08
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-10 10:46:56 +0300

    :construction: admin main content header menu added

--- src/components/adminContentHeader/AdminContentHeader.jsx
+++ src/components/adminContentHeader/AdminContentHeader.jsx
@@ -1,26 +1,19 @@
 import "./admincontentheader.css";
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 export default function AdminContentHeader() {
-    const [activeTab, setActiveTab] = useState("tab1");
 
   return (
     <div className="main-content-header">
       <h3>All Posts posted either via wordpress or gracesoft</h3>
-      <div>
-        <input type="radio" id="tab1" className="tab" {...activeTab === "tab1" ? "checked" : ""}/>
-        <label htmlFor="tab1">
-          <i class="fa fa-code"></i> All Posts
-        </label>
-        <input type="radio" id="tab2" className="tab"/>
-        <label htmlFor="tab2">
-          <i class="fa fa-code"></i> Categories
-        </label>
-        <input type="radio" id="tab3" className="tab"/>
-        <label htmlFor="tab3">
-          <i class="fa fa-code"></i> Devidends
-        </label>
+      <div className="admin-header-menu-container">
+          <ul className="admin-header-menu">
+              <li><Link to="/" className="header-link">Posts</Link></li>
+              <li><Link to="/" className="header-link">Devotion</Link></li>
+              <li><Link to="/" className="header-link">Announcement</Link></li>
+              <li><Link to="/" className="header-link">News</Link></li>
+          </ul>
       </div>
     </div>
   );
