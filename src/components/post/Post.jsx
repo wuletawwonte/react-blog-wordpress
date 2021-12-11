fde2ab3e3c2cfa527cbaaa06772c058c6c41555f
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-11 12:20:31 +0300

    :bug: fixed a responsive design issue

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,32 +1,44 @@
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./post.css";
 import Editor from "../editor/Editor";
 
 export default function Post(props) {
-  const { title, excerpt, featured_image_url, author_id } = props;
-  const [authorData, setAuthorData] = useState(null);
+    const { title, excerpt, featured_image_url, author_id } = props;
+    const [authorData, setAuthorData] = useState(null);
 
-  useEffect(() => {
-      axios(`/wp-json/wp/v2/users/${author_id}`)
-        .then((res) => {
-            setAuthorData(res.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-  }, [author_id])
+    useEffect(() => {
+        axios(`/wp-json/wp/v2/users/${author_id}`)
+            .then((res) => {
+                setAuthorData(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [author_id]);
 
-  return (
-    <div className="post-container">
-      { featured_image_url ?
-      <img src={featured_image_url} alt={title} className="post-img-content"  /> : ""}
-      <div className="post-text-content">
-        <h2 dangerouslySetInnerHTML={{ __html: title }}></h2>
-        <p dangerouslySetInnerHTML={{ __html: excerpt.substring(0, 150) }}></p>
-        {authorData ? <Editor editorData={authorData} /> : ""}
-      </div>
-    </div>
-  );
+    return (
+        <div className="post-container">
+            <div className="post-img-container">
+                {featured_image_url ? (
+                    <img
+                        src={featured_image_url}
+                        alt={title}
+                        className="post-img-content"
+                    />
+                ) : (
+                    ""
+                )}
+            </div>
+            <div className="post-text-content">
+                <h2 dangerouslySetInnerHTML={{ __html: title }}></h2>
+                <p
+                    dangerouslySetInnerHTML={{
+                        __html: excerpt.substring(0, 150),
+                    }}
+                ></p>
+                {authorData ? <Editor editorData={authorData} /> : ""}
+            </div>
+        </div>
+    );
 }
