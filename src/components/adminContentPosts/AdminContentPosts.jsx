1cba6ccc5b547b26bc871cb3669397ba3bce0704
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-11 22:05:56 +0300

    :recycle: refactored by creating AdminContentPosts component

---
+++ src/components/adminContentPosts/AdminContentPosts.jsx
@@ -0,0 +1,44 @@
+import "./admincontentposts.css";
+import axios from "axios";
+import { useState, useEffect } from 'react';
+import Post from "../post/Post"; 
+
+export default function AdminContentPosts() {
+    const [posts, setPosts] = useState({
+        isLoaded: false,
+        data: null,
+        category: null,
+    });
+
+    useEffect(() => {
+        axios("/wp-json/wp/v2/posts")
+            .then((res) => {
+                setPosts({
+                    isLoaded: true,
+                    data: res.data,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    return (
+        <div>
+            {posts.isLoaded
+                ? posts.data.map((post) => {
+                      return (
+                          <div key={post.id}>
+                              <Post
+                                  featured_image_url={post.featured_image_url}
+                                  title={post.title.rendered}
+                                  excerpt={post.excerpt.rendered}
+                                  author_id={post.author}
+                              />
+                          </div>
+                      );
+                  })
+                : "Loading"}
+        </div>
+    );
+}
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,25 +1,11 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Post from "../../components/post/Post";
 import AdminNavbar from "../../components/adminNavbar/AdminNavbar";
 import "./dashboard.css";
 import Menu from "../../components/menu/Menu";
 import SideMenuUser from "../../components/sideMenuUser/SideMenuUser";
 import AdminContentHeader from "../../components/adminContentHeader/AdminContentHeader";
+import AdminContentPosts from "../../components/adminContentPosts/AdminContentPosts";
 
 export default function Dashboard() {
-  const [posts, setPosts] = useState({});
-
-  useEffect(() => {
-    axios
-      .get("/wp-json/wp/v2/posts")
-      .then((res) => {
-        setPosts({ data: res.data, isLoaded: true });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
 
   return (
     <>
@@ -32,20 +18,7 @@ export default function Dashboard() {
           </div>
           <div className="item main-content">
             <AdminContentHeader />
-            {posts.isLoaded
-              ? posts.data.map((post) => {
-                  return (
-                    <div key={post.id}>
-                      <Post
-                        featured_image_url={post.featured_image_url}
-                        title={post.title.rendered}
-                        excerpt={post.excerpt.rendered}
-                        author_id={post.author}
-                      />
-                    </div>
-                  );
-                })
-              : "Loading"}
+            <AdminContentPosts />
           </div>
         </div>
       </div>
