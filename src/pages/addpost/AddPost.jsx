83a5d989b68530df9f8e15fc6f48f27902a6ebfe
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-12-14 17:58:50 +0300

    :art: post adding form works

--- src/pages/addpost/AddPost.jsx
+++ src/pages/addpost/AddPost.jsx
@@ -1,9 +1,70 @@
 import "./addpost.css";
+import { Formik } from "formik";
 
 export default function AddPost() {
     return (
-        <div>
-            <h2>Add Post</h2>
+        <div className="add-post-container">
+            <div className="add-post-header">
+                <h2>Add Post</h2>
+            </div>
+            <Formik
+                initialValues={{ title: "", content: "" }}
+                validate={(values) => {
+                    const errors = {};
+                    if (!values.title) {
+                        errors.title = "Post title is required";
+                    }
+                    if (!values.content) {
+                        errors.content =
+                            "The content of a post cannot be empty";
+                    }
+                    return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting,
+                    /* and other goodies */
+                }) => (
+                    <form onSubmit={handleSubmit} className="add-post-form">
+                        <input
+                            type="text"
+                            name="title"
+                            className="title-input"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.title}
+                            placeholder="title ..."
+                        />
+                        {errors.title && touched.title && errors.title}
+                        <textarea
+                            type="text"
+                            name="content"
+                            className="content-input"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.content}
+                            rows="5"
+                            placeholder="content ..."
+                        />
+                        {errors.content && touched.content && errors.content}
+                        <button type="submit" className="add-post-btn" disabled={isSubmitting}>
+                            Submit
+                        </button>
+                    </form>
+                )}
+            </Formik>
         </div>
-    )
+    );
 }
